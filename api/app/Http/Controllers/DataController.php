a00c1bdfbaedd272b94e5882fc17cc9fb8e09ec3
Faaizz <fr33ziey@gmail.com>
2021-01-29 21:11:12 +0100

    feat: Implement DataController logic

--- api/app/Http/Controllers/DataController.php
+++ api/app/Http/Controllers/DataController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Validator;
 
 class DataController extends Controller
 {
@@ -16,9 +18,47 @@ class DataController extends Controller
      */
     public function data(Request $request){
 
+        // Validation
+        $validator = Validator::make($request->all(), [
+            'from' => 'required',
+            'to' => 'required'
+        ]);
+
+        // Validation Failed
+        if ($validator->fails()){
+            return response()->json([
+                "msg" => "Please specify correct values"
+            ], 400);
+        }
+
+        // Validation Successful
+        // Get date range from request
+        $from_date= $request->input("from");
+        $to_date= $request->input("to");
+
+        // Return JSON response
         return response()->json([
-            "msg"=> "Holla"
+            "data"=> $this->fetchData($from_date, $to_date)
         ], 200);
+
+    }
+
+    /**
+     * Fetch requested data from CoinDesk API
+     * @param String $from_date
+     * @param String $to_date
+     * @return String
+     */
+    public function fetchData($from_date, $to_date){
+
+        $response = Http::get(
+            "https://api.coindesk.com/v1/bpi/historical/close.json"
+            ."?start=". $from_date . "&end=" . $to_date 
+        );
+
+        // Return useful data
+        return $response->json()['bpi'];
         
     }
+
 }
